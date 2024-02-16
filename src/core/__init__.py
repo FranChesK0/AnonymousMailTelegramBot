7db8ee998fff5fa60e98b53c06a2944b41fd8b47
FranChesKo <khl_doss@mail.ru>
2024-02-17 02:10:59 +0800

    Add project settings

---
+++ src/core/__init__.py
@@ -0,0 +1,3 @@
+from .project_settings import settings
+
+__all__ = ["settings"]
---
+++ src/core/project_settings.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+project_directory: str = os.path.dirname(os.path.abspath(__file__)).removesuffix(
+    os.path.join("src", "core")
+)
+
+
+class BotSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(project_directory, ".env"), env_prefix="BOT_"
+    )
+
+    token: str = "xxx"
+
+
+class Settings(BaseSettings):
+    project_directory: str = project_directory
+    debug: bool = "-d" in sys.argv or "--debug" in sys.argv
+    bot: BotSettings = BotSettings()
+
+
+settings: Settings = Settings()
