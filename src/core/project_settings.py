398c4019c618c57780dafd14719886b68b52e80f
FranChesKo <khl_doss@mail.ru>
2024-02-17 10:09:54 +0800

    Add mail api to environment variables

--- src/core/project_settings.py
+++ src/core/project_settings.py
@@ -8,6 +8,14 @@ project_directory = os.path.dirname(os.path.abspath(__file__)).removesuffix(
 )
 
 
+class MailSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(project_directory, ".env"), env_prefix="MAIL_"
+    )
+
+    api: str = "xxx"
+
+
 class BotSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=os.path.join(project_directory, ".env"), env_prefix="BOT_"
@@ -19,6 +27,7 @@ class BotSettings(BaseSettings):
 class Settings(BaseSettings):
     project_directory: str = project_directory
     debug: bool = "-d" in sys.argv or "--debug" in sys.argv
+    mail: MailSettings = MailSettings()
     bot: BotSettings = BotSettings()
 
 
