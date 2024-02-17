2e31b63319eef449352550748f06998dacbfe28c
FranChesKo <khl_doss@mail.ru>
2024-02-18 01:30:35 +0800

    Add redis env variables

--- src/core/project_settings.py
+++ src/core/project_settings.py
@@ -24,11 +24,23 @@ class BotSettings(BaseSettings):
     token: str = "xxx"
 
 
+class RedisSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(project_directory, ".env"), env_prefix="REDIS_"
+    )
+
+    host: str = "xxx"
+    port: int = 1000
+    password: str = "xxx"
+    db: int = 0
+
+
 class Settings(BaseSettings):
     project_directory: str = project_directory
     debug: bool = "-d" in sys.argv or "--debug" in sys.argv
     mail: MailSettings = MailSettings()
     bot: BotSettings = BotSettings()
+    redis: RedisSettings = RedisSettings()
 
 
 settings = Settings()
